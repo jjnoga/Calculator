c0320aa3f1a84c1c4f9271c63cbb6d2bd897d1a9
jjnoga <jjnoga@quinnipiac.edu>
2024-02-07 18:59:32 -0500

    Second commit: Allow user to specify operator used

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,20 @@ public class Calculator {
         System.out.println("Please input two whole numbers.");
         int number1 = input.nextInt();
         int number2 = input.nextInt();
-        System.out.println(number1 + number2);
-        
+
+        int arith = 3;
+        while (arith >= 3 || arith <= 0) {
+            System.out.println("Please input which operator you would like to perform on the two numbers. Type 1 for addition, 2 for subtraction.");
+            arith = input.nextInt();
+        }
+        switch (arith) {
+            case 1:
+                System.out.println(number1 + number2);
+                break;
+            case 2:
+                System.out.println(number1 - number2);
+                break;
+        }
+
     }
 }
